8940256f650aef183669cff822ae4173274b2e47
abhiandthetruth <abhiandthetruth@gmail.com>
2019-12-18 19:01:53 +0530

    implemented template inheritence and added some templates

--- jar/urls.py
+++ jar/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'jar'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics')
 ]
