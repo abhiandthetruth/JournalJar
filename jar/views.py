b9c2ded01fae7c62b86cf3cf662851d147851d92
abhiandthetruth <abhiandthetruth@gmail.com>
2019-12-18 23:31:34 +0530

    topic list

--- jar/views.py
+++ jar/views.py
@@ -9,3 +9,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics' : topics}
     return render(request, 'jar/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    print(topic)
+    #minus for reverse order
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'jar/topic.html', context)
